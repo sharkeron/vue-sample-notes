c516cecd980d19b1781c9dbf1f4758d163d45258
Konstantin <sharkeron@gmail.com>
2017-11-21 00:22:50 +0200

    First commit

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, index) in quotes" :key="quote.id" @click.native="deleteQuote(index)">{{ quote }}
+    </app-quote>
+  </div>
+  <!-- /.container -->
+</template>
+
+<script>
+  import Quote from './Quote.vue'
+
+  export default {
+    props: ['quotes'],
+    components: {
+      appQuote: Quote
+    },
+    methods: {
+      deleteQuote (index) {
+        this.$emit('quoteDelete', index)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/newQuote.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote</label>
+        <textarea class="form-control" rows="3" v-model="quote"></textarea>
+      </div>
+
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+      </div>
+    </form>
+  </div>
+  <!-- /.container -->
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        quote: ''
+      }
+    },
+    methods: {
+      createNew () {
+        if (this.quote.length > 0) {
+          this.$emit('quoteAdded', this.quote)
+          this.quote = ''
+        } else {
+          return alert('Your quote not have a text! Please add some text.')
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
